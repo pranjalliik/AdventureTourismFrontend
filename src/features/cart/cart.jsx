2c4673c1721c4af3694cfebb987c2121e126b441
pranjalliik <131262798+pranjalliik@users.noreply.github.com>
2024-05-06 19:19:42 +0530

    Update cart.jsx

--- src/features/cart/cart.jsx
+++ src/features/cart/cart.jsx
@@ -62,7 +62,7 @@ import { Link } from "react-router-dom";
   }}
 />
 <div className="h-screen bg-gray-100 pt-16">
-  <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+  <h1 className="mb-10 text-center text-2xl font-bold">Liked Tours</h1>
   <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
     <div className="rounded-lg md:w-2/3">
 
